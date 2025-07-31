2d39300eb03b6fdc115486002de4268fa8cc0719
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-31 13:00:48 -0700

    Fix padding and add <main> element

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,17 +2,20 @@ import Image from "next/image";
 
 export default function Hero(): React.JSX.Element {
   return (
-    <header>
-      <nav aria-label="Global" className="flex justify-center p-6">
-        <Image
-          src="/glaam-logo.png"
-          alt="Logo of the Greater Los Angeles Mensa Area Chapter."
-          className="w-sm"
-          width={600}
-          height={200}
-	  priority
-        />
-      </nav>
-    </header>
+    <>
+      <header>
+	<nav aria-label="Global" className="flex justify-center px-12 py-6">
+          <Image
+            src="/glaam-logo.png"
+            alt="Logo of the Greater Los Angeles Mensa Area Chapter."
+            className="w-3xs lg:w-xs"
+            width={600}
+            height={200}
+	    priority
+          />
+	</nav>
+      </header>
+      <main></main>
+    </>
   );
 }
