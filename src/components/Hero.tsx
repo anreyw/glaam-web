50fa9947077261e467d1467044bdcf7506c23045
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-31 13:22:20 -0700

    Fix nav styling to accommodate new link

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero(): React.JSX.Element {
   return (
     <>
       <header>
-	<nav aria-label="Global" className="flex justify-center px-12 py-6">
+	<nav aria-label="Global" className="flex flex-col sm:flex-row justify-between items-center px-12 py-6">
           <Image
             src="/glaam-logo.png"
             alt="Logo of the Greater Los Angeles Mensa Area Chapter."
