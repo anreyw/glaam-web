2c9e7bf5029e5bcac0a54e95e7632f6c052f16fd
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-31 10:42:33 -0700

    Import Next.js Image component
    Use it for the Mensa/GLAAM logo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,12 @@
+import Image from "next/image";
+
 export default function Hero(): React.JSX.Element {
   return (
     <header>
       <nav
 	aria-label="Global"
       >
-	Nav
+	<Image />
       </nav>
     </header>
   );
