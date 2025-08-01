7137ed873206a2063ccd93977f07d28577faf16d
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-31 21:02:50 -0700

    Comment out decorative blur background

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,43 +3,69 @@ import Link from "next/link";
 
 export default function Hero(): React.JSX.Element {
   return (
-    <div className="p-6 sm:p-12">
-      <header>
-        <nav
-          aria-label="Global"
-          className="flex flex-col sm:flex-row justify-between items-center"
-        >
-          <Image
-            src="/glaam-logo.png"
-            alt="Logo of the Greater Los Angeles Mensa Area Chapter."
-            className="w-3xs lg:w-xs"
-            width={600}
-            height={200}
-            priority
-          />
-          <Link href="#" className="hidden sm:inline-block">
-            Member Resources
-          </Link>
-        </nav>
-      </header>
-      <main>
-        <h1 className="text-4xl sm:text-5xl font-semibold py-12">
-          Where Brilliance Belongs
-        </h1>
-        <p>
-          Greater Los Angeles Area Mensa (or “GLAAM“) is a local chapter of
-          American Mensa, Ltd. Our territory covers all of Los Angeles and Kern
-          and Inyo counties, along with portions of San Bernardino and Riverside
-          and Ventura counties. Mensa (TM) is an international society which
-          welcomes all persons who have tested within the top two percentile of
-          the population in intelligence. We enjoy a variety of ‘smart’
-          activities in our local communities, as exemplified in the photos
-          below. We currently have some 150,000 members worldwide, but we are
-          always looking for more. Click the Testing link in the menu below to
-          see if you or a loved one is qualified to join us!
-        </p>
-        <a href="https://www.us.mensa.org/join/">Join Now</a>
-      </main>
-    </div>
+    <>
+      {/* DECORATIVE BLUR BACKGROUND
+      <div
+        aria-hidden="true"
+        className="absolute inset-x-0 -top-40 -z-10 transform-gpu overflow-hidden blur-3xl sm:-top-80"
+      >
+        <div
+          style={{
+            clipPath:
+              "polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)",
+          }}
+          className="relative left-[calc(50%-11rem)] aspect-1155/678 w-144.5 -translate-x-1/2 rotate-30 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%-30rem)] sm:w-288.75"
+        />
+      </div>
+	*/}
+      
+
+      <div className="p-6 sm:p-12">
+        <header>
+          <nav
+            aria-label="Global"
+            className="flex flex-col sm:flex-row justify-between items-center gap-4"
+          >
+            <Image
+              src="/glaam-logo.png"
+              alt="Logo of the Greater Los Angeles Mensa Area Chapter."
+              className="w-3xs lg:w-xs"
+              width={600}
+              height={200}
+              priority
+            />
+            <Link
+              href="#"
+              className="rounded-md order-first sm:order-none sm:inline-block bg-blue-300 text-sm px-4 py-2 hover:bg-blue-200 hover:outline hover:outline-blue-300 hover:outline-offset-2"
+            >
+              Click Here for Members-Only Resources
+            </Link>
+          </nav>
+        </header>
+        <main>
+          <h1 className="text-6xl sm:text-5xl py-12">
+            Where Brilliance Belongs
+          </h1>
+          <p className="">
+            Greater Los Angeles Area Mensa (or “GLAAM“) is a local chapter of
+            American Mensa, Ltd. Our territory covers all of Los Angeles and
+            Kern and Inyo counties, along with portions of San Bernardino and
+            Riverside and Ventura counties. Mensa (TM) is an international
+            society which welcomes all persons who have tested within the top
+            two percentile of the population in intelligence. We enjoy a variety
+            of ‘smart’ activities in our local communities, as exemplified in
+            the photos below. We currently have some 150,000 members worldwide,
+            but we are always looking for more. Click the Testing link in the
+            menu below to see if you or a loved one is qualified to join us!
+          </p>
+          <a
+            href="https://www.us.mensa.org/join/"
+            className="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 my-6 block sm:inline-block"
+          >
+            Join Now
+          </a>
+        </main>
+      </div>
+    </>
   );
 }
