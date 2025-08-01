87ce8069fc8d35147dcd4ffa165fd0439888c57e
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-07-31 21:51:18 -0700

    Update main Hero text and styling

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -44,23 +44,21 @@ export default function Hero(): React.JSX.Element {
           <h1 className="text-6xl sm:text-5xl py-12">
             Where Brilliance Belongs
           </h1>
-          <p className="">
+          <p className="text-lg">
             Greater Los Angeles Area Mensa (or “GLAAM“) is a local chapter of
             American Mensa, Ltd. Our territory covers all of Los Angeles and
             Kern and Inyo counties, along with portions of San Bernardino and
-            Riverside and Ventura counties. Mensa (TM) is an international
+            Riverside and Ventura counties. Mensa&trade; is an international
             society which welcomes all persons who have tested within the top
             two percentile of the population in intelligence. We enjoy a variety
-            of ‘smart’ activities in our local communities, as exemplified in
-            the photos below. We currently have some 150,000 members worldwide,
-            but we are always looking for more. Click the Testing link in the
-            menu below to see if you or a loved one is qualified to join us!
+            of ‘smart’ activities in our local communities. We currently have
+            some 150,000 members worldwide, but we are always looking for more.
           </p>
           <a
             href="https://www.us.mensa.org/join/"
-            className="rounded-md bg-blue-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 my-6 block sm:inline-block"
+            className="rounded-md bg-blue-600 px-3.5 py-2.5 text-md font-semibold text-white shadow-xs hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 my-6 block sm:inline-block text-center"
           >
-            Join Now
+            Learn How to Join
           </a>
         </main>
       </div>
