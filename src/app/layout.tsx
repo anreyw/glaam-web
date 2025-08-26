0c334617e9c5838019575731103a2a9d81db9307
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-08-26 13:32:39 -0700

    Install Inter font as a Local Font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,20 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import "./globals.css";
+
+const inter = localFont({
+  src: [
+    {
+      path: "../../public/InterVariable.ttf",
+      style: "normal",
+    },
+    {
+      path: "../../public/InterVariable-Italic.ttf",
+      style: "italic",
+    },
+  ],
+  variable: "--font-inter",
+});
 
 export const metadata: Metadata = {
   title: "Greater LA Area Mensa",
@@ -12,9 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-    >
+    <html lang="en" className={inter.className}>
       <body>
         <div>{children}</div>
       </body>
