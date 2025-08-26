1b88f1aba29c8986c588a00b6cbb3cb2f90a0639
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-08-26 13:25:27 -0700

    Remove Geist font
    I want to switch back to Inter.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,4 @@
 import type { Metadata } from "next";
-import { GeistSans } from "geist/font/sans";
-import { GeistMono } from "geist/font/mono";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -16,7 +14,6 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${GeistSans.variable} ${GeistMono.variable} antialiased`}
     >
       <body>
         <div>{children}</div>
