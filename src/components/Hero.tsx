9b372238afa617894d3e476544de8b0119a6ee64
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-08-27 09:58:29 -0700

    Implement new layout idea

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,25 +3,19 @@ import JoinButton from "@/components/JoinButton";
 
 export default function Hero(): React.JSX.Element {
   return (
-    <>
-      <div className="p-6 sm:p-12">
-        <main className="flex flex-col items-center">
-          <Image
-            src="/glaam-logo.png"
-            alt="Logo of the Greater Los Angeles Mensa Area Chapter."
-            className="w-3xs lg:w-xs"
-            width={600}
-            height={200}
-            priority
-          />
-          <div className="flex flex-col items-center max-w-3xl">
-            <h1 className="text-6xl sm:text-5xl py-12">
-              Where Brilliance Belongs
-            </h1>
-            <JoinButton />
-          </div>
-        </main>
+    <main className="flex flex-col h-screen justify-center items-center">
+      <Image
+        src="/glaam-logo.png"
+        alt="Logo of the Greater Los Angeles Mensa Area Chapter."
+        className="w-3xs lg:w-xs"
+        width={600}
+        height={200}
+        priority
+      />
+      <div className="flex flex-col items-center max-w-3xl">
+        <h1 className="text-6xl sm:text-5xl py-12">Where Brilliance Belongs</h1>
+        <JoinButton />
       </div>
-    </>
+    </main>
   );
 }
