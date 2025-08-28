8771d550434ee96a3a1cc7abb61cb1e1bf56733d
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-08-28 12:46:23 -0700

    Change Testimonals handle key to memberSince
    For GLAAM testimonials, we want to emphasize member commitment and not
    their social media handles.

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -5,7 +5,7 @@ const testimonials = [
     body: "Laborum quis quam. Dolorum et ut quod quia. Voluptas numquam delectus nihil. Aut enim doloremque et ipsam.",
     author: {
       name: "Leslie Alexander",
-      handle: "lesliealexander",
+      memberSince: "Member since 2012",
       imageUrl:
         "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     },
@@ -14,7 +14,7 @@ const testimonials = [
     body: "Anim sit consequat culpa commodo eu do nisi commodo ut aute aliqua. Laborum esse duis tempor consectetur officia mollit fugiat. Exercitation qui elit minim minim quis fugiat ex.",
     author: {
       name: "Michael Foster",
-      handle: "michaelfoster",
+      memberSince: "michaelfoster",
       imageUrl:
         "https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     },
@@ -23,7 +23,7 @@ const testimonials = [
     body: "Consequatur ut atque. Itaque nostrum molestiae id veniam eos cumque.",
     author: {
       name: "Dries Vincent",
-      handle: "driesvincent",
+      memberSince: "driesvincent",
       imageUrl:
         "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     },
@@ -32,7 +32,7 @@ const testimonials = [
     body: "Excepteur consectetur deserunt id incididunt veniam mollit officia sint qui aute duis sit cillum. Reprehenderit fugiat amet aliqua in commodo minim sunt laborum.",
     author: {
       name: "Lindsay Walton",
-      handle: "lindsaywalton",
+      memberSince: "lindsaywalton",
       imageUrl:
         "https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     },
@@ -41,7 +41,7 @@ const testimonials = [
     body: "Distinctio facere aliquam est qui atque sint molestias ad. Fuga consequuntur asperiores voluptatum ipsum.",
     author: {
       name: "Courtney Henry",
-      handle: "courtneyhenry",
+      memberSince: "courtneyhenry",
       imageUrl:
         "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     },
@@ -50,7 +50,7 @@ const testimonials = [
     body: "Nam nesciunt dolorem dolor asperiores sint. Incidunt molestiae quis deleniti vitae ut in earum delectus iusto.",
     author: {
       name: "Tom Cook",
-      handle: "tomcook",
+      memberSince: "tomcook",
       imageUrl:
         "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     },
@@ -59,7 +59,7 @@ const testimonials = [
     body: "Voluptas quos itaque ipsam in voluptatem est. Iste eos blanditiis repudiandae. Earum deserunt enim molestiae ipsum perferendis.",
     author: {
       name: "Whitney Francis",
-      handle: "whitneyfrancis",
+      memberSince: "whitneyfrancis",
       imageUrl:
         "https://images.unsplash.com/photo-1517365830460-955ce3ccd263?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     },
@@ -68,7 +68,7 @@ const testimonials = [
     body: "Aliquid dolore praesentium ratione. Cumque ea officia repellendus laboriosam. Vitae quod id explicabo non sunt.",
     author: {
       name: "Leonard Krasner",
-      handle: "leonardkrasner",
+      memberSince: "leonardkrasner",
       imageUrl:
         "https://images.unsplash.com/photo-1519345182560-3f2917c472ef?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     },
@@ -77,7 +77,7 @@ const testimonials = [
     body: "Molestias ea earum quos nostrum doloremque sed. Quaerat quasi aut velit incidunt excepturi rerum voluptatem minus harum.",
     author: {
       name: "Floyd Miles",
-      handle: "floydmiles",
+      memberSince: "floydmiles",
       imageUrl:
         "https://images.unsplash.com/photo-1463453091185-61582044d556?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
     },
@@ -100,7 +100,7 @@ export default function Testimonials(): JSX.Element {
           <div className="-mt-8 sm:-mx-4 sm:columns-2 sm:text-[0] lg:columns-3">
             {testimonials.map((testimonial) => (
               <div
-                key={testimonial.author.handle}
+                key={testimonial.author.name}
                 className="pt-8 sm:inline-block sm:w-full sm:px-4"
               >
                 <figure className="rounded-2xl bg-gray-50 p-8 text-sm/6 dark:bg-white/2.5">
@@ -117,7 +117,7 @@ export default function Testimonials(): JSX.Element {
                       <div className="font-semibold text-gray-900 dark:text-white">
                         {testimonial.author.name}
                       </div>
-                      <div className="text-gray-600 dark:text-gray-400">{`@${testimonial.author.handle}`}</div>
+                      <div className="text-gray-600 dark:text-gray-400">{`${testimonial.author.memberSince}`}</div>
                     </div>
                   </figcaption>
                 </figure>
