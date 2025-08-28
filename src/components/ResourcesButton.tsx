1d08e84215f7fbe082fe8980f09f31e229e368aa
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-08-28 12:27:59 -0700

    Create Resources Button stub

---
+++ src/components/ResourcesButton.tsx
@@ -0,0 +1,12 @@
+import { JSX } from "react";
+
+export default function ResourcesButton(): JSX.Element {
+  return (
+    <a
+      href="#"
+      className="rounded-md bg-pink-600 px-3.5 py-2.5 text-xl text-white shadow-xs hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 my-6 block sm:inline-block text-center"
+    >
+       Resources Button (stub)
+    </a>
+  )
+}
