c84ba1a053fc446bd8cea6d7bbbf452e1795d549
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-08-28 12:36:08 -0700

    Fix colors on ResourcesButton

--- src/components/ResourcesButton.tsx
+++ src/components/ResourcesButton.tsx
@@ -4,7 +4,7 @@ export default function ResourcesButton(): JSX.Element {
   return (
     <a
       href="#"
-      className="rounded-md bg-pink-600 px-3.5 py-2.5 text-xl text-white shadow-xs hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 block sm:inline-block text-center"
+      className="rounded-md bg-pink-600 px-3.5 py-2.5 text-xl text-white shadow-xs hover:bg-pink-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600 block sm:inline-block text-center"
     >
        Resources Button (stub)
     </a>
