81e15d9e8e7fd29d487d5b0bc5b3b6454b50383f
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-08-28 12:19:36 -0700

    Add Testimonials to Homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Hero from "@/components/Hero";
+import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
-  return <Hero />;
+  return (
+    <>
+      <Hero />
+      <Testimonials />
+    </>
+  );
 }
