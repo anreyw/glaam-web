35e546265fdf9f76a9550148408ebe1899f78b61
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-08-28 10:44:51 -0700

    Add Hero description paragraph with layout fix

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,19 +4,21 @@ import Background from "@/components/Background";
 
 export default function Hero(): React.JSX.Element {
   return (
-    <main className="flex flex-col h-screen justify-start items-center gap-12 py-8">
+    <main className="flex flex-col h-screen justify-start items-center gap-4 py-8 px-6">
       <Background />
       <Image
         src="/glaam-logo.png"
         alt="Logo of the Greater Los Angeles Mensa Area Chapter."
-        className="w-3xs lg:w-xs"
+        className="w-3xs"
         width={600}
         height={200}
         priority
       />
-      <h1 className="text-5xl lg:text-6xl text-center mx-8">
-        Where Brilliance Belongs
-      </h1>
+      <h1 className="text-4xl text-center">Where Brilliance Belongs</h1>
+      <p>
+        Greater Los Angeles Area Mensa (GLAAM) is a local chapter of the largest
+        and oldest high-IQ society in the world.
+      </p>
       <JoinButton />
     </main>
   );
