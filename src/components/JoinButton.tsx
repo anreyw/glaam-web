8482625506a6a66f22cadf3fc04a64c2080af322
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-08-28 18:34:53 -0700

    Improve link to include text fragment

--- src/components/JoinButton.tsx
+++ src/components/JoinButton.tsx
@@ -3,7 +3,7 @@ import { JSX } from "react";
 export default function JoinButton(): JSX.Element {
   return (
     <a
-      href="https://www.us.mensa.org/join/"
+      href="https://www.us.mensa.org/join#:~:text=How%20to%20Join%20Mensa"
       className="rounded-md bg-blue-600 px-3.5 py-2.5 text-xl text-white shadow-xs hover:bg-blue-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-blue-600 block sm:inline-block text-center"
     >
       Learn How to Join
