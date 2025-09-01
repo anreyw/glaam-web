2ea1211ba5c78287f82cabe8f047f9001aba5939
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-08-31 20:11:51 -0700

    Update Testimonials text

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -63,10 +63,10 @@ export default function Testimonials(): JSX.Element {
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl text-center">
           <h2 className="text-base/7 font-semibold text-blue-600">
-            Member Testimonials
+            Testimonials
           </h2>
           <p className="mt-2 text-4xl font-medium tracking-tight text-balance text-gray-900 sm:text-5xl">
-            In Our Own Words
+            From Curiosity to Connection
           </p>
         </div>
         <div className="mx-auto mt-16 flow-root max-w-2xl sm:mt-20 lg:mx-0 lg:max-w-none">
