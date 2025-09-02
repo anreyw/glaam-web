ca6fccd9bdf2bff6376df993c48cd61cb7540c9f
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-09-02 08:49:12 -0700

    Add correct Footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -81,7 +81,7 @@ export default function Footer() {
           ))}
         </div>
         <p className="mt-8 text-center text-sm/6 text-gray-600 md:order-1 md:mt-0 dark:text-gray-400">
-          &copy; 2024 Your Company, Inc. All rights reserved.
+          &copy; 2025 Greater Los Angeles Area Mensa. All rights reserved.
         </p>
       </div>
     </footer>
