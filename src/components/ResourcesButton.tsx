11f1c9244fbea71182e8a76d0a113fd08afac237
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-09-03 12:39:09 -0700

    Update Link to Members route on Resource Button

--- src/components/ResourcesButton.tsx
+++ src/components/ResourcesButton.tsx
@@ -1,12 +1,13 @@
 import { JSX } from "react";
+import Link from "next/link";
 
 export default function ResourcesButton(): JSX.Element {
   return (
-    <a
-      href="#"
+    <Link
+      href="/members"
       className="rounded-md bg-pink-600 px-3.5 py-2.5 text-xl text-white shadow-xs hover:bg-pink-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-pink-600 block sm:inline-block text-center"
     >
        Member Resources
-    </a>
+    </Link>
   )
 }
