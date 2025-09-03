920b833523b14e92a56e56b4bf4772a631009dfa
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-09-03 12:43:06 -0700

    Add horizontal margin

--- src/app/members/layout.tsx
+++ src/app/members/layout.tsx
@@ -3,5 +3,7 @@ export default function MembersLayout({
 }: {
   children: React.ReactNode;
 }) {
-  return <div className="pt-24 sm:pt-36 flex justify-center">{children}</div>;
+  return (
+    <div className="mt-24 sm:mt-36 mx-6 flex justify-center">{children}</div>
+  );
 }
