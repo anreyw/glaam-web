5798fe70a2dbaa3dbcd6174b15b41b09df59a6f9
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-09-03 12:41:45 -0700

    Add return Link to members route

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function Members() {
-  return <p className="text-3xl">This page is under construction.</p>
+  return <p className="text-3xl">This page is under construction. <Link className="underline text-blue-600"href="/">Go back.</Link></p>
 }
