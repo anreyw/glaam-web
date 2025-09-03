f45bf673386d38464f884eaf3cd2a81237176b33
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-09-03 10:23:38 -0700

    Add About component stub

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,12 +2,14 @@ import Hero from "@/components/Hero";
 import Testimonials from "@/components/Testimonials";
 import Background from "@/components/Background";
 import Footer from "@/components/Footer";
+import About from "@/components/About";
 
 export default function Home() {
   return (
     <>
       <Background />
       <Hero />
+      <About />
       <Testimonials />
       <Footer />
     </>
