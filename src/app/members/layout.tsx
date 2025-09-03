dd846bf9ba7212896276b11536469bc61e72b0e6
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-09-03 12:35:23 -0700

    Add Members Route
    - add Members layout
    - add Members page (automatically creating route)
    - add page construction message

---
+++ src/app/members/layout.tsx
@@ -0,0 +1,7 @@
+export default function MembersLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div className="pt-24 flex justify-center">{children}</div>;
+}
---
+++ src/app/members/page.tsx
@@ -0,0 +1,3 @@
+export default function Members() {
+  return <p className="text-3xl">This page is under construction.</p>
+}
