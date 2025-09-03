aca5437d5289abadcc1ae20bdd5a25a323d61419
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-09-03 12:57:25 -0700

    Add type and prettify

--- src/app/members/page.tsx
+++ src/app/members/page.tsx
@@ -1,5 +1,16 @@
+import { JSX } from "react";
+
 import Link from "next/link";
 
-export default function Members() {
-  return <p className="text-3xl">This page is under construction. <Link className="underline text-blue-600"href="/">Go back.</Link></p>
+export default function Members(): JSX.Element {
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="text-3xl">This page is under construction.</p>
+      <p className="text-3xl">
+        <Link className="underline text-blue-600 hover:no-underline" href="/">
+          Go back.
+        </Link>
+      </p>
+    </div>
+  );
 }
