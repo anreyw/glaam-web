a56714561f8a6cb52d89e02bb18e94774d9d0b32
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-09-04 09:55:08 -0700

    Add CallToAction to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import Testimonials from "@/components/Testimonials";
 import Background from "@/components/Background";
 import Footer from "@/components/Footer";
 import About from "@/components/About";
+import CallToAction from "@/components/CallToAction";
 
 export default function Home() {
   return (
@@ -11,6 +12,7 @@ export default function Home() {
       <Hero />
       <About />
       <Testimonials />
+      <CallToAction />
       <Footer />
     </>
   );
