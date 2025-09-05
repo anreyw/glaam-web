6bfcb1c289189354a766a3fc1740a33ba601c842
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-09-05 05:10:45 -0700

    Place JoinButton and remove filler text

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -11,17 +11,8 @@ export default function CallToAction(): React.JSX.Element {
           Ready to find your people? Start your Mensa journey now.
         </p>
       </hgroup>
-      <p className="mx-auto mt-6 max-w-xl text-lg/8 text-pretty text-gray-600 dark:text-gray-400">
-        Incididunt sint fugiat pariatur cupidatat consectetur sit cillum anim id
-        veniam aliqua proident excepteur commodo do ea.
-      </p>
       <div className="mt-8 flex justify-center">
-        <a
-          href="#"
-          className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500"
-        >
-          Get started
-        </a>
+        <JoinButton />
       </div>
     </section>
   );
