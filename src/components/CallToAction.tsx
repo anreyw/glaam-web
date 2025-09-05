c0cc1523647c7002557930b8f5b9ae3eb6a3ab0f
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-09-05 04:45:46 -0700

    Reduce boldness on CallToAction description

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -5,7 +5,7 @@ export default function CallToAction(): React.JSX.Element {
         <h2 className="text-base/7 font-semibold text-blue-600 dark:text-indigo-400">
           Join Greater Los Angeles Area Mensa
         </h2>
-        <p className="mt-2 text-4xl font-semibold tracking-tight text-balance text-gray-900 sm:text-5xl dark:text-white">
+        <p className="mt-2 text-4xl font-medium tracking-tight text-balance text-gray-900 sm:text-5xl dark:text-white">
           Boost your productivity. Start using our app today.
         </p>
       </hgroup>
