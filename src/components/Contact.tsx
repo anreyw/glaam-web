00afe908321859d0634c5298683f3dbbaf30a7db
Anrey Wang <139710025+anreyw@users.noreply.github.com>
2025-09-17 10:35:02 -0700

    Change Contact component template

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -2,111 +2,161 @@ import { JSX } from "react";
 
 export default function Contact(): JSX.Element {
   return (
-    <div className="bg-white py-8 sm:py-16 dark:bg-gray-900">
-      <div className="mx-auto max-w-7xl px-6 lg:px-8">
-        <div className="mx-auto max-w-2xl divide-y divide-gray-100 lg:mx-0 lg:max-w-none dark:divide-white/10">
-          <div className="grid grid-cols-1 gap-10 py-16 lg:grid-cols-3">
-            <div>
-              <h2 className="text-4xl font-semibold tracking-tight text-pretty text-gray-900 dark:text-white">
-                Get in touch
-              </h2>
-              <p className="mt-4 text-base/7 text-gray-600 dark:text-gray-400">
-                Quam nunc nunc eu sed. Sed rhoncus quis ultricies ac
-                pellentesque.
-              </p>
-            </div>
-            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:col-span-2 lg:gap-8">
-              <div className="rounded-2xl bg-gray-50 p-10 dark:bg-gray-800/50">
-                <h3 className="text-base/7 font-semibold text-gray-900 dark:text-white">
-                  Collaborate
-                </h3>
-                <dl className="mt-3 space-y-1 text-sm/6 text-gray-600 dark:text-gray-400">
-                  <div>
-                    <dt className="sr-only">Email</dt>
-                    <dd>
-                      <a
-                        href="mailto:collaborate@example.com"
-                        className="font-semibold text-indigo-600 dark:text-indigo-400"
-                      >
-                        collaborate@example.com
-                      </a>
-                    </dd>
-                  </div>
-                  <div className="mt-1">
-                    <dt className="sr-only">Phone number</dt>
-                    <dd>+1 (555) 905-2345</dd>
-                  </div>
-                </dl>
+    <div className="relative isolate bg-white dark:bg-gray-900">
+      <div className="mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2">
+        <div className="relative px-6 pt-24 pb-20 sm:pt-32 lg:static lg:px-8 lg:py-48">
+          <div className="mx-auto max-w-xl lg:mx-0 lg:max-w-lg">
+            <h2 className="text-4xl font-semibold tracking-tight text-pretty text-gray-900 sm:text-5xl dark:text-white">
+              Get in touch
+            </h2>
+            <p className="mt-6 text-lg/8 text-gray-600 dark:text-gray-400">
+              Proin volutpat consequat porttitor cras nullam gravida at. Orci
+              molestie a eu arcu. Sed ut tincidunt integer elementum id sem.
+              Arcu sed malesuada et magna.
+            </p>
+            <dl className="mt-10 space-y-4 text-base/7 text-gray-600 dark:text-gray-300">
+              <div className="flex gap-x-4">
+                <dt className="flex-none">
+                  <span className="sr-only">Address</span>
+                </dt>
+                <dd>
+                  545 Mavis Island
+                  <br />
+                  Chicago, IL 99191
+                </dd>
               </div>
-              <div className="rounded-2xl bg-gray-50 p-10 dark:bg-gray-800/50">
-                <h3 className="text-base/7 font-semibold text-gray-900 dark:text-white">
-                  Press
-                </h3>
-                <dl className="mt-3 space-y-1 text-sm/6 text-gray-600 dark:text-gray-400">
-                  <div>
-                    <dt className="sr-only">Email</dt>
-                    <dd>
-                      <a
-                        href="mailto:press@example.com"
-                        className="font-semibold text-indigo-600 dark:text-indigo-400"
-                      >
-                        press@example.com
-                      </a>
-                    </dd>
-                  </div>
-                  <div className="mt-1">
-                    <dt className="sr-only">Phone number</dt>
-                    <dd>+1 (555) 905-3456</dd>
-                  </div>
-                </dl>
+              <div className="flex gap-x-4">
+                <dt className="flex-none">
+                  <span className="sr-only">Telephone</span>
+                </dt>
+                <dd>
+                  <a
+                    href="tel:+1 (555) 234-5678"
+                    className="hover:text-gray-900 dark:hover:text-white"
+                  >
+                    +1 (555) 234-5678
+                  </a>
+                </dd>
               </div>
-              <div className="rounded-2xl bg-gray-50 p-10 dark:bg-gray-800/50">
-                <h3 className="text-base/7 font-semibold text-gray-900 dark:text-white">
-                  Join our team
-                </h3>
-                <dl className="mt-3 space-y-1 text-sm/6 text-gray-600 dark:text-gray-400">
-                  <div>
-                    <dt className="sr-only">Email</dt>
-                    <dd>
-                      <a
-                        href="mailto:careers@example.com"
-                        className="font-semibold text-indigo-600 dark:text-indigo-400"
-                      >
-                        careers@example.com
-                      </a>
-                    </dd>
-                  </div>
-                  <div className="mt-1">
-                    <dt className="sr-only">Phone number</dt>
-                    <dd>+1 (555) 905-4567</dd>
-                  </div>
-                </dl>
+              <div className="flex gap-x-4">
+                <dt className="flex-none">
+                  <span className="sr-only">Email</span>
+                </dt>
+                <dd>
+                  <a
+                    href="mailto:hello@example.com"
+                    className="hover:text-gray-900 dark:hover:text-white"
+                  >
+                    hello@example.com
+                  </a>
+                </dd>
               </div>
-              <div className="rounded-2xl bg-gray-50 p-10 dark:bg-gray-800/50">
-                <h3 className="text-base/7 font-semibold text-gray-900 dark:text-white">
-                  Say hello
-                </h3>
-                <dl className="mt-3 space-y-1 text-sm/6 text-gray-600 dark:text-gray-400">
-                  <div>
-                    <dt className="sr-only">Email</dt>
-                    <dd>
-                      <a
-                        href="mailto:hello@example.com"
-                        className="font-semibold text-indigo-600 dark:text-indigo-400"
-                      >
-                        hello@example.com
-                      </a>
-                    </dd>
-                  </div>
-                  <div className="mt-1">
-                    <dt className="sr-only">Phone number</dt>
-                    <dd>+1 (555) 905-5678</dd>
-                  </div>
-                </dl>
-              </div>
-            </div>
+            </dl>
           </div>
         </div>
+        <form
+          action="#"
+          method="POST"
+          className="px-6 pt-20 pb-24 sm:pb-32 lg:px-8 lg:py-48"
+        >
+          <div className="mx-auto max-w-xl lg:mr-0 lg:max-w-lg">
+            <div className="grid grid-cols-1 gap-x-8 gap-y-6 sm:grid-cols-2">
+              <div>
+                <label
+                  htmlFor="first-name"
+                  className="block text-sm/6 font-semibold text-gray-900 dark:text-white"
+                >
+                  First name
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    id="first-name"
+                    name="first-name"
+                    type="text"
+                    autoComplete="given-name"
+                    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500"
+                  />
+                </div>
+              </div>
+              <div>
+                <label
+                  htmlFor="last-name"
+                  className="block text-sm/6 font-semibold text-gray-900 dark:text-white"
+                >
+                  Last name
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    id="last-name"
+                    name="last-name"
+                    type="text"
+                    autoComplete="family-name"
+                    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="email"
+                  className="block text-sm/6 font-semibold text-gray-900 dark:text-white"
+                >
+                  Email
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    id="email"
+                    name="email"
+                    type="email"
+                    autoComplete="email"
+                    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="phone-number"
+                  className="block text-sm/6 font-semibold text-gray-900 dark:text-white"
+                >
+                  Phone number
+                </label>
+                <div className="mt-2.5">
+                  <input
+                    id="phone-number"
+                    name="phone-number"
+                    type="tel"
+                    autoComplete="tel"
+                    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500"
+                  />
+                </div>
+              </div>
+              <div className="sm:col-span-2">
+                <label
+                  htmlFor="message"
+                  className="block text-sm/6 font-semibold text-gray-900 dark:text-white"
+                >
+                  Message
+                </label>
+                <div className="mt-2.5">
+                  <textarea
+                    id="message"
+                    name="message"
+                    rows={4}
+                    className="block w-full rounded-md bg-white px-3.5 py-2 text-base text-gray-900 outline-1 -outline-offset-1 outline-gray-300 placeholder:text-gray-400 focus:outline-2 focus:-outline-offset-2 focus:outline-indigo-600 dark:bg-white/5 dark:text-white dark:outline-white/10 dark:placeholder:text-gray-500 dark:focus:outline-indigo-500"
+                    defaultValue={""}
+                  />
+                </div>
+              </div>
+            </div>
+            <div className="mt-8 flex justify-end">
+              <button
+                type="submit"
+                className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-center text-sm font-semibold text-white shadow-xs hover:bg-indigo-500 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 dark:bg-indigo-500 dark:hover:bg-indigo-400 dark:focus-visible:outline-indigo-500"
+              >
+                Send message
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
     </div>
   );
